101879d17b358eb83c9ee7a29cb64a682ccf35cb
GracieEne <enehgrace2000@gmail.com>
2025-09-03 15:10:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+  const toggle = document.querySelector('.dropdown-toggle');
+  const menu = document.querySelector('.dropdown-menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (!toggle.contains(e.target) && !menu.contains(e.target)) {
+      menu.classList.remove('show');
+    }
+  });
